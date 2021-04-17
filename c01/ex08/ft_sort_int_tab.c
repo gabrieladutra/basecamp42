9f77b85790c87a1de982d9e23ea0c1904ac0e11c
gdias-du <gdias-du@student.42sp.org.br>
2021-04-17 12:00:25 +0000

    Added c01 resolution

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdias-du <gdias-du@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/08 23:32:52 by gdias-du          #+#    #+#             */
+/*   Updated: 2021/04/10 03:21:54 by gdias-du         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int temp;
+	int j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[i])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
